cf61f7b826e735d3bb64f969efb8bca6fe98dd2a
kparasch <konstantinos.paraschou@cern.ch>
2024-02-12 14:15:29 +0100

    increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ECIX_tools',
-    version="2.0",
+    version="2.1",
     description='Tools for e-cloud simulations in xsuite',
     long_description='Tools for e-cloud simulations in xsuite',
     author='K. Paraschou',
